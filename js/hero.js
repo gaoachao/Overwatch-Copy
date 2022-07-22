0e14347cbedf81fe57499c52439b54230c795825
gaoachao <1227448581@qq.com>
2022-07-22 15:31:29 +0800

    update 7.22

---
+++ js/hero.js
@@ -0,0 +1,271 @@
+const dva = document.getElementById("dva");
+const aolisha = document.getElementById("aolisha");
+const laiyinhate = document.getElementById("laiyinhate");
+const luba = document.getElementById("luba");
+const wensidun = document.getElementById("wensidun");
+const chaliya = document.getElementById("chaliya");
+const pohuaiqiu = document.getElementById("pohuaiqiu");
+const xigema = document.getElementById("xigema");
+const badisite = document.getElementById("badisite");
+const aishen = document.getElementById("aishen");
+const baolei = document.getElementById("baolei");
+const yuanshi = document.getElementById("yuanshi");
+const banzang = document.getElementById("banzang");
+const kuangshu = document.getElementById("kuangshu");
+const maikelei = document.getElementById("maikelei");
+const mei = document.getElementById("mei");
+const falaozhiying = document.getElementById("falaozhiying");
+const sishen = document.getElementById("sishen");
+const shibing76 = document.getElementById("shibing76");
+const heiying = document.getElementById("heiying");
+const zhixuzhiguang = document.getElementById("zhixuzhiguang");
+const tuobiang = document.getElementById("tuobiang");
+const liekong = document.getElementById("liekong");
+const heibaihe = document.getElementById("heibaihe");
+const bulijita = document.getElementById("bulijita");
+const luxiao = document.getElementById("luxiao");
+const tianshi = document.getElementById("tianshi");
+const moyila = document.getElementById("moyila");
+const chanyata = document.getElementById("chanyata");
+const Echo = document.getElementById("Echo");
+const moritiequan = document.getElementById("moritiequan");
+const anna = document.getElementById("anna");
+const Text = document.getElementById("Text");
+const Tu = document.getElementById("Tu");
+const textTitle = document.getElementById("textTitle");
+const textCentent = document.getElementById("textCentent");
+
+dva.onmouseover = function () {
+  Tu.className = "thehero-lunbo-centet-tu dva";
+  textTitle.innerText = "D.Va";
+  textCentent.innerText =
+    "A former pro gamer who now pilots a state-of-the-art mech in defense of her homeland.";
+};
+aolisha.onmouseover = function () {
+  Tu.className = "thehero-lunbo-centet-tu aolisha";
+  textTitle.innerText = "ORISA";
+  textCentent.innerText = "A mechanical peacekeeper, built to defend the people of Numbani.";
+};
+laiyinhate.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu laiyinhate";
+  textTitle.innerText = "REINHARDT";
+  textCentent.innerText =
+    "A champion of a bygone age, who lives by the knightly codes of valor, justice, and courage.";
+};
+luba.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu luba";
+  textTitle.innerText = "ROADHOG";
+  textCentent.innerText = "A ruthless killer with tremendous strength and a reputation for cruelty and wanton destruction.";
+};
+wensidun.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu wensidun";
+  textTitle.innerText = "WINSTON";
+  textCentent.innerText =
+    "A super-intelligent, genetically engineered gorilla, scientist, and champion for humanity's potential.";
+};
+chaliya.onmouseover = function () {
+	Text.style.display = 'block'
+	Tu.style.display = 'block'
+	Tu.className = 'thehero-lunbo-centet-tu chaliya'
+	textTitle.innerText = 'ZARYA'
+	textCentent.innerText = "One of the world's strongest women, who sacrificed personal glory to protect her country."
+}
+pohuaiqiu.onmouseover = function () {
+	Text.style.display = 'block'
+	Tu.style.display = 'block'
+	Tu.className = 'thehero-lunbo-centet-tu pohuaiqiu'
+	textTitle.innerText = 'WRECKING BALL'
+	textCentent.innerText = 'This rolling tank is piloted by a genetically-engineered hamster from the moon.'
+}
+xigema.onmouseover = function () {
+	Text.style.display = 'block'
+	Tu.style.display = 'block'
+	Tu.className = 'thehero-lunbo-centet-tu xigema'
+	textTitle.innerText = 'SIGMA'
+	textCentent.innerText = 'An eccentric astrophysicist transformed into a living weapon by an orbital experiment gone awry.'
+}
+badisite.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu badisite";
+  textTitle.innerText = "BAPTISTE";
+  textCentent.innerText = "An elite combat medic armed with an arsenal of experimental healing devices.";
+};
+aishen.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu aishen";
+  textTitle.innerText = "ASHE";
+  textCentent.innerText = "The leader of the notorious Deadlock Gang menaces the American Southwest.";
+};
+baolei.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu baolei";
+  textTitle.innerText = "BASTION";
+  textCentent.innerText =
+    "A transforming robot who explores the world; fascinated by nature, but wary of humanity.";
+};
+moritiequan.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu moritiequan";
+  textTitle.innerText = "DOOMFIST";
+  textCentent.innerText =
+    "A tactician who uses his intelligence, charisma and brute force to build a stronger world.";
+};
+yuanshi.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu yuanshi";
+  textTitle.innerText = "GENJI";
+  textCentent.innerText =
+    "A cyborg ninja and deadly warrior who has found peace with his mechanical body.";
+};
+banzang.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu banzang";
+  textTitle.innerText = "HANZO";
+  textCentent.innerText = "A lethal bowman and assassin without peer.";
+};
+kuangshu.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu kuangshu";
+  textTitle.innerText = "JUNKRAT";
+  textCentent.innerText =
+    "An explosives-obsessed freak who lives to cause chaos and destruction.";
+};
+maikelei.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu maikelei";
+  textTitle.innerText = "CASSIDY";
+  textCentent.innerText = "An outlaw gunslinger who doles out justice on his own terms.";
+};
+mei.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu mei";
+  textTitle.innerText = "MEI";
+  textCentent.innerText = "A specialist in weather manipulation who has taken up the fight to preserve the environment.";
+};
+falaozhiying.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu falaozhiying";
+  textTitle.innerText = "PHARAH";
+  textCentent.innerText =
+    "A decorated soldier who patrols the skies in her experimental Raptora combat suit.";
+};
+sishen.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu sishen";
+  textTitle.innerText = "REAPER";
+  textCentent.innerText = "A mercenary and ruthless killer who hunts the former agents of Overwatch.";
+};
+shibing76.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu shibing76";
+  textTitle.innerText = "SOLDIER: 76";
+  textCentent.innerText = "A vigilante who will stop at nothing to bring the enemies of Overwatch to justice.";
+};
+heiying.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu heiying";
+  textTitle.innerText = "SOMBRA";
+  textCentent.innerText = "A notorious hacker, on the hunt for secrets—and the power they bring.";
+};
+zhixuzhiguang.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu zhixuzhiguang";
+  textTitle.innerText = "SYMMETRA";
+  textCentent.innerText =
+    "An architech who manipulates hard-light constructs to build her perfect, ordered world.";
+};
+tuobiang.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu tuobiang";
+  textTitle.innerText = "TORBJÖRN";
+  textCentent.innerText = "An ingenious engineer who forges weapons systems on the battlefield.";
+};
+liekong.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu liekong";
+  textTitle.innerText = "TRACER";
+  textCentent.innerText =
+    "A former Overwatch agent, time-jumping adventurer, and irrepressible force for good.";
+};
+heibaihe.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu heibaihe";
+  textTitle.innerText = "WIDOWMAKER";
+  textCentent.innerText =
+    "A perfect assassin: patient, ruthlessly efficient, and without emotion or remorse.";
+};
+anna.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu anna";
+  textTitle.innerText = "ANA";
+  textCentent.innerText =
+    "This Overwatch stalwart has emerged from years of seclusion to defend a new generation.";
+};
+bulijita.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu bulijita";
+  textTitle.innerText = "BRIGITTE";
+  textCentent.innerText = "An armored warrior, searching for adventure and the chance to help those in need.";
+};
+luxiao.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu luxiao";
+  textTitle.innerText = "LÚCIO";
+  textCentent.innerText = "An international celebrity who fights for social change through his music and actions.";
+};
+tianshi.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu tianshi";
+  textTitle.innerText = "MERCY";
+  textCentent.innerText =
+    "A guardian angel, peerless healer, brilliant medical researcher, and staunch advocate for peace.";
+};
+moyila.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu moyila";
+  textTitle.innerText = "MOIRA";
+  textCentent.innerText =
+    "A brilliant geneticist who pursues scientific discovery unburdened by ethics.";
+};
+chanyata.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu chayata";
+  textTitle.innerText = "ZENYATTA";
+  textCentent.innerText = "An omnic monk who wanders the world in search of spiritual enlightenment.";
+};
+Echo.onmouseover = function () {
+  Text.style.display = "block";
+  Tu.style.display = "block";
+  Tu.className = "thehero-lunbo-centet-tu Echo";
+  textTitle.innerText = "ECHO";
+  textCentent.innerText = "An evolutionary robot programmed with rapidly adapting artificial intelligence, Echo represents the cutting edge of technology.";
+};
